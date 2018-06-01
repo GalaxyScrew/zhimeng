ecc1129aaa49312ccd330f4ec58896553ab40fea
Jayme <1048154174@qq.com>
2018-06-01 16:06:44 +0800

    Add files via upload

---
+++ removexss.php
@@ -0,0 +1,48 @@
+<?php
+    function RemoveXSS($val) {
+       $val = preg_replace('/([\x00-\x08,\x0b-\x0c,\x0e-\x19])/', '', $val);
+       //根据W3C的标准，以上16进制的字符是不被允许出现在XML文件中
+       $search = 'abcdefghijklmnopqrstuvwxyz';
+       $search .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+       $search .= '1234567890!@#$%^&*()';
+       $search .= '~`";:?+/={}[]-_|\'\\';
+       for ($i = 0; $i < strlen($search); $i++) {
+          $val = preg_replace('/(&#[xX]0{0,8}'.dechex(ord($search[$i])).';?)/i', $search[$i], $val);
+          // i 是忽略大小写， 将16进制的ASCII字符替换成utf-8字符
+          $val = preg_replace('/(&#0{0,8}'.ord($search[$i]).';?)/', $search[$i], $val); // with a ;
+          //将ASCII字符转换为utf-8字符
+       }
+
+       $ra1 = array('javascript', 'vbscript', 'expression', 'applet', 'meta', 'xml', 'blink', 'link', 'style', 'script', 'embed', 'object', 'iframe', 'frame', 'frameset', 'ilayer', 'layer', 'bgsound', 'title', 'base');
+       //需要过滤的html标签
+       $ra2 = array('onabort', 'onactivate', 'onafterprint', 'onafterupdate', 'onbeforeactivate', 'onbeforecopy', 'onbeforecut', 'onbeforedeactivate', 'onbeforeeditfocus', 'onbeforepaste', 'onbeforeprint', 'onbeforeunload', 'onbeforeupdate', 'onblur', 'onbounce', 'oncellchange', 'onchange', 'onclick', 'oncontextmenu', 'oncontrolselect', 'oncopy', 'oncut', 'ondataavailable', 'ondatasetchanged', 'ondatasetcomplete', 'ondblclick', 'ondeactivate', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop', 'onerror', 'onerrorupdate', 'onfilterchange', 'onfinish', 'onfocus', 'onfocusin', 'onfocusout', 'onhelp', 'onkeydown', 'onkeypress', 'onkeyup', 'onlayoutcomplete', 'onload', 'onlosecapture', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove', 'onmouseout', 'onmouseover', 'onmouseup', 'onmousewheel', 'onmove', 'onmoveend', 'onmovestart', 'onpaste', 'onpropertychange', 'onreadystatechange', 'onreset', 'onresize', 'onresizeend', 'onresizestart', 'onrowenter', 'onrowexit', 'onrowsdelete', 'onrowsinserted', 'onscroll', 'onselect', 'onselectionchange', 'onselectstart', 'onstart', 'onstop', 'onsubmit', 'onunload');
+       //需要过滤的html事件
+       $ra = array_merge($ra1, $ra2);
+
+       $found = true; 
+       while ($found == true) {
+          $val_before = $val;
+          for ($i = 0; $i < sizeof($ra); $i++) {//对每个单词进行过滤
+             $pattern = '/';
+             for ($j = 0; $j < strlen($ra[$i]); $j++) {//构造正则表达式
+                if ($j > 0) {
+                   $pattern .= '(';
+                   $pattern .= '(&#[xX]0{0,8}([9ab]);)';//无效字符
+                   $pattern .= '|';
+                   $pattern .= '|(&#0{0,8}([9|10|13]);)';//无效字符
+                   $pattern .= ')*';
+                }
+                $pattern .= $ra[$i][$j];
+             }
+             $pattern .= '/i';
+             $replacement = substr($ra[$i], 0, 2).'<x>'.substr($ra[$i], 2);//在检测到的敏感单词之间加入<x>
+             $val = preg_replace($pattern, $replacement, $val); 
+             if ($val_before == $val) {
+                $found = false;
+             }
+          }
+       }
+       return $val;
+    }
+
+?>
